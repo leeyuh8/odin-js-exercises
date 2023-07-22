ef6ecdfca3c00cb2433f3cbed3df52ccbea5c7de
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-21 19:29:51 -0700

    Code passes test 1 to 5

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,14 @@
 
 const repeatString = function(string, num) {
-    let str = "";
-    for (let i = 1; i <= num; i++) {
-        str += string;
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        let str = "";
+        for (let i = 1; i <= num; i++) {
+            str += string;
+        }
+        return str;
     }
-    return str;
 }
 
 // Do not edit below this line
