e6730fce264e2cd313318ba70a2725440bfe27eb
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-21 19:27:07 -0700

    Code passes tests 1 to 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
 
-};
+const repeatString = function(string, num) {
+    let str = "";
+    for (let i = 1; i <= num; i++) {
+        str += string;
+    }
+    return str;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
