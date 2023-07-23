9dcbfa03ca1e123928363a03d9f54f50d1af0f0f
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-22 18:08:48 -0700

    Exercise 2 and 3 pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+/*
+-Split each character into the input string into an array.
+-write loop that creates a new string from the array by
+concatenating from the last to first element.
+-return string.
+*/
 
+const reverseString = function(input) {
+    let string = input.split("");
+    let stringReversed = "";
+    for (let i = 0; i <= string.length - 1; i++) {
+        stringReversed = string[i] + stringReversed;
+    };
+    return stringReversed;
 };
 
 // Do not edit below this line
