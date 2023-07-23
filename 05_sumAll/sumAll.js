d7b03a6bf87dafb5f3d052dd4c47e6edcd124362
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-23 14:11:58 -0700

    Exercise 5 passes tests 1-2

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+/*
+- write a for loop where:
+    - i starts at the start value
+    - i stops at the end value
+    - code adds the current i value to the finalSum variable
+*/
 
+const sumAll = function(numStart, numEnd) {
+    let finalSum = 0;
+    for (let i = numStart; i <= numEnd; i++) {
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
