9f1b40aea83e98c028a4259cc5112db4569fc527
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-23 16:13:00 -0700

    Exercise 6 passes tests 1

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+/*
+Conditions:
+    - all leap years are divisible by 4
+    - for all test years that end in '00', it is only a
+    leap year if it is divisible by 400
+    - code should test the '00' year exception first
+*/
 
+const leapYears = function(year) {
+    let lastTwoDigits = +year.toString().slice(-2);
+    // if (lastTwoDigits === 00 
+    // && year % 400 === 0 
+    // && year % 4 === 0) return 'true';
+    if (year % 4 === 0) return true;
 };
 
 // Do not edit below this line
