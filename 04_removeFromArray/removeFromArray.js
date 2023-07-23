3ed4086715fa1d109fd8282b0852c3d673119fa7
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-07-22 19:09:17 -0700

    Exercise 4 passes test 1

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+/* 
+-find the index of where numToRemove is in the array
+-use splice to remove that number by its index
+-return new array
 
+update take multiple numToRemove arguments:
+- *arguments can be treated like arrays
+   (it has indexes and uses the .length method)*
+- convert previous code to for loop that runs
+the code for each numToRemove
+*/
+
+const removeFromArray = function(array, num) {
+    let index = array.indexOf(num);
+    array.splice(index, 1);
+    return array;
 };
 
 // Do not edit below this line
