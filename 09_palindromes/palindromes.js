759927709a653687ea440f4e0c1b495ce5209e70
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-26 17:32:41 -0700

    Exercise 9 passes test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  let original = str.split('');
+  let reverse = original.reverse();
+  
+  for (let i = 0; i < original.length - 1; i++) {
+    if (original[i] !== reverse[i]) return false;
+  };
 
+  return true;
 };
 
 // Do not edit below this line
