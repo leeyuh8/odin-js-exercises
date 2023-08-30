631dfe87707e7d5ba4e9ce0173297b5e61eb6fc3
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-08-29 21:11:55 -0700

    Exercise 10 passes tests 1-6

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,10 +1,10 @@
 const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+
   let arr = [1,1];
-  
   for (let i = 2; i <= n - 1; i++) {
     arr[i] = arr[i-2] + arr[i-1];
   };
-
   return arr[arr.length - 1];
 };
 
