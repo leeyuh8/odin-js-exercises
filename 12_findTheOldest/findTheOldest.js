f9c099b7f9ad4a44d60d0be6a75e2ef7b6695324
Leeyuh <134454347+leeyuh8@users.noreply.github.com>
2023-09-01 16:36:17 -0700

    Exercise 12 passes test 1

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  arr.forEach( person => 
+    person.yearsLived = person.yearOfDeath - person.yearOfBirth);
+  arr.sort( (a, b) => 
+    b.yearsLived - a.yearsLived);
+  return arr[0];
 };
+
+/*
+x- first, copy the arr and to each element add a property 
+named "yearsLived" that subtracts the death by birth
+x- use arr.sort to ordr from oldest to youngest
+based on yearsLived
+- remove the yearsLived data and return the first/oldest 
+person element
+ */
 
 // Do not edit below this line
 module.exports = findTheOldest;
